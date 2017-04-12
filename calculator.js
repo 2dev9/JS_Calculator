a152e362a999e93b58908ce490f1aa8adbc0916a
howell.devon@gmail.com <howell.devon@gmail.com>
2017-04-11 20:54:14 -0400

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function add() {
+	console.log(document.getElementById('num1').value); 
+}
+
+function subtract(){
+	console.log(6-384305);
+}
+
+function multiply(){
+	console.log(32*432);
+}
+
+function divide(){
+	console.log(348/5);
+}
